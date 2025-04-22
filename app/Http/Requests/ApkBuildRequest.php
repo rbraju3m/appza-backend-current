70a88acc7c998f63d65b3656cd017e527adeb728
Right_Brain_Raju <raju.rightbrainsolution@gmail.com>
2025-04-22 17:03:48 +0600

    build process rearange

--- app/Http/Requests/ApkBuildRequest.php
+++ app/Http/Requests/ApkBuildRequest.php
@@ -23,7 +23,7 @@ class ApkBuildRequest extends FormRequest
      *
      * @return array
      */
-    public function rules()
+    /*public function rules()
     {
         $rules = [
             'app_name' => 'string',
@@ -51,5 +51,46 @@ class ApkBuildRequest extends FormRequest
         }
 
         return $rules;
+    }*/
+
+    public function rules()
+    {
+        $rules = [
+            'app_name' => $this->isAndroidSelected() ? 'required|string' : 'string',
+            'app_logo' => 'required|string',
+            'app_splash_screen_image' => 'required|string',
+            'site_url' => 'required|url',
+            'license_key' => 'required',
+            'email' => 'required|email',
+            'plugin_slug' => 'required',
+            'platform' => 'required|array'
+        ];
+
+        $iosFields = ['ios_issuer_id', 'ios_key_id', 'ios_p8_file_content', 'ios_team_id'];
+
+        // Check if any iOS specific field is filled
+        $iosFieldFilled = false;
+        foreach($iosFields as $field){
+            if ($this->filled($field)) {
+                $iosFieldFilled = true;
+            }
+        }
+
+        foreach($iosFields as $field) {
+            $rules[$field] = $iosFieldFilled ? 'required' : 'nullable';
+        }
+
+        return $rules;
+    }
+
+    /**
+     * Check if Android is selected in the platform array
+     *
+     * @return bool
+     */
+    protected function isAndroidSelected()
+    {
+        $platform = $this->input('platform', []);
+        return is_array($platform) && in_array('android', $platform);
     }
 }
