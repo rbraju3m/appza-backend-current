446c4bc73fce5be16f56d01786bef7e2870b78ee
rbraju3m <rbraju3m@gmail.com>
2025-08-25 15:25:01 +0600

    Update bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Sentry\Laravel\Integration;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
@@ -13,11 +13,17 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // global middleware, if any
     })
     ->withExceptions(function (Exceptions $exceptions) {
         Integration::handles($exceptions);
     })
-    ->withConsoleKernel(App\Console\Kernel::class)
     ->create();
 
+// 💡 This now executes correctly
+$app->singleton(
+    Illuminate\Contracts\Console\Kernel::class,
+    App\Console\Kernel::class
+);
+
+return $app;
