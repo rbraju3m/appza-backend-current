74a5a33e2e1313473e403f9cbbccb41d9241c36a
rbraju3m <rbraju3m@gmail.com>
2025-09-07 15:32:18 +0600

    Update database migration

---
+++ database/migrations/2025_09_07_152909_change_in_datetime_free_trial_request_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('appza_free_trial_request', function (Blueprint $table) {
+            $table->dateTime('expiration_date')->change();
+            $table->dateTime('grace_period_date')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('appza_free_trial_request', function (Blueprint $table) {
+            //
+        });
+    }
+};
