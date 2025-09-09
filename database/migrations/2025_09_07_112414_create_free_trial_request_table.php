770557728d405194c6357d481798b6c54499ea15
rbraju3m <rbraju3m@gmail.com>
2025-09-09 13:51:54 +0600

    Update database migration

--- database/migrations/2025_09_07_112414_create_free_trial_request_table.php
+++ database/migrations/2025_09_07_112414_create_free_trial_request_table.php
@@ -38,6 +38,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('free_trial_request');
+        Schema::dropIfExists('appza_free_trial_request');
     }
 };
