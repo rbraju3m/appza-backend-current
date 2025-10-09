2a375650b1f41340f94865a3db737ee81e3757b2
rbraju3m <rbraju3m@gmail.com>
2025-10-09 12:37:23 +0600

    fixed :: css

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer footer-area">
     <div class="container">
-        <span class="text-muted">© 2024  <b style="font-weight: bold">Appza </b>, All rights reserved.</span>
+        <span class="text-muted">© {{date('Y')}}  <b style="font-weight: bold">Appza </b>, All rights reserved.</span>
     </div>
 </footer>
