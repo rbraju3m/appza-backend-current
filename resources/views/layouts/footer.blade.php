def1460a60a0c3f16119b1253ce925dd5d7453cc
rbraju3m <rbraju3m@gmail.com>
2025-10-13 17:44:14 +0600

    footer update

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer footer-area">
     <div class="container">
-        <span class="text-muted">© {{date('Y')}}  <b style="font-weight: bold">Appza </b>, All rights reserved.</span>
+        <span class="text-muted">© 2023 - {{date('Y')}}  <b style="font-weight: bold">Appza </b>, All rights reserved.</span>
     </div>
 </footer>
